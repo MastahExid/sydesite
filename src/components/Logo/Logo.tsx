4a9c5478387fa300c51fe9f8a5da14c33f41d26e
exid <exidbiz@gmail.com>
2025-12-29 03:41:48 +0400

    Update Logo.tsx

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -16,7 +16,7 @@ export const Logo = (props: Props) => {
   return (
     /* eslint-disable @next/next/no-img-element */
     <img
-      alt="Payload Logo"
+      alt="Syde"
       width={193}
       height={34}
       loading={loading}
